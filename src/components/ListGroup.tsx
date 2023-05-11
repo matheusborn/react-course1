2577dd3bba8ad8899b97a05c58e24b4d97e21f78
Matheus Barros <matheusborn@hotmail.com>
2023-05-10 21:31:24 -0300

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,17 +2,11 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li key={index} className="list-group-item">
