b3562d84c3d103c0c47b6c947f4753b5f5ed1fd2
Matheus Barros <matheusborn@hotmail.com>
2023-05-10 21:26:06 -0300

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
